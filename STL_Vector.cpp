e86e82e5c4c7af8925913da367bd76c6c122f4da
HarshithaPadarthi <86092847+HarshithaPadarthi@users.noreply.github.com>
2023-02-14 21:43:13 +0530

    Update STL_Vector.cpp

--- STL_Vector.cpp
+++ STL_Vector.cpp
@@ -1,4 +1,29 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main()
+int main(){
+  //vector<data_type>vector_name;
+  //declaring vectors
+  vector<int>a;//empty vector {}
+  vector<int>b={1,2,3,4,5};
+  //to create a vector having n elements and all the elements are 10's
+  int n;//5
+  cin>>n;
+  vector<int>c(n,10);
+  for(int i=0;i<n;i++){
+    cout<<c[i]<<" ";//10 10 10 10 10
+  }
+  //to read vector without giving its size
+  for(int i=0;i<n;i++){//n=4 , i/p: 1 2 3 4
+    int x;
+    cin>>x;
+    a.push_back(x);//{1,2,3,4}
+  }
+  //to read vector by giving its size
+  int m;
+  cin>>m;//6
+  vector<int>d(m);
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+  }
+}
